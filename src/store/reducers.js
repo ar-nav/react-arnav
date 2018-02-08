270bf4663c4815f09412fa518a4826e3b895bee5
febri <febriliandoputra@gmail.com>
2018-02-08 22:25:51 +0700

    new update, before rebase

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,18 +1,27 @@
 const initialState = {
-  drawerOpen: false
+  drawerOpen: false,
+  suggestions: [
+    { label: 'Afghanistan' },
+    { label: 'Aland Islands' },
+    { label: 'Albania' },
+  ],
 }
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'ADD_ARRAY_STORE':
-      return {...state, }
+      return { ...state }
     case 'TOGGLE_DRAWER':
       return {
         ...state,
-        drawerOpen: action.payload.toggle
+        drawerOpen: action.payload.toggle,
+      }
+    case 'GET_SUGGESTIONS':
+      return {
+        ...state,
       }
     default:
-      return state;
+      return state
   }
 }
 
