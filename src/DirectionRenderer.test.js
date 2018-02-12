53bd7c931787d5100a2bd92917a11ac3641fa7c4
wahibhanii <wahib.hanii@gmail.com>
2018-02-12 13:54:08 +0700

    checkpoint AR-testing

--- src/DirectionRenderer.test.js
+++ src/DirectionRenderer.test.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import expect, { createSpy } from 'expect'
 import geolib from 'geolib'
 import { shallow, mount, configure } from 'enzyme'
+import renderer from 'react-test-renderer';
 
 import Switch from 'material-ui/Switch'
 import InfoIcon from 'material-ui-icons/Info'
@@ -10,15 +11,10 @@ import { FormControlLabel, FormGroup } from 'material-ui/Form'
 import Button from 'material-ui/Button'
 import Menu, { MenuItem } from 'material-ui/Menu'
 
-import { directionRendererFactory, getAngle, compassHeading, DirectionRenderer } from './components/direction/DirectionRenderer'
+
+import { directionRendererFactory, getAngle, compassHeading } from './components/direction/DirectionRenderer'
 
 describe ('Direction Renderer', () => {
-  const mesh = {
-    position: {},
-    scale: {},
-    rotation: {},
-}
-const material = {}
 const DirectionRenderer = directionRendererFactory({
     THREE: {
         Camera: createSpy(),
@@ -33,9 +29,29 @@ const DirectionRenderer = directionRendererFactory({
     initializeRenderer: createSpy().andReturn({ render: createSpy() }),
     requestAnimationFrame: createSpy()
 });
-  it('Should lbasdfkjbasg', () => {
+  // it('Should renders correctly', () => {
+  //   // const div = document.createElement('div');
+  //   let props = {
+  //     coords: {
+  //       latitude: 0,
+  //       longitude: 0
+  //     },
+  //     targetLoc : {
+  //       latitude: 100,
+  //       longitude: 200,
+  //     },
+  //     currentLoc :{
+  //       latitude: 0,
+  //       longitude: 0
+  //     }
 
-  })
+  //   }
+  //   // let directionRenderer = ReactDOM.render(<DirectionRenderer {...props} />, div);
+  //   const tree = renderer
+  //   .create(<DirectionRenderer {...props} />)
+  //   .toJSON();
+  // expect(tree).toMatchSnapshot();
+  // })
 
   it('Get Angle should get correct angle', () => {
     let targetLoc = {
@@ -56,20 +72,6 @@ const DirectionRenderer = directionRendererFactory({
       gamma = 30
     let result = compassHeading(alpha, beta, gamma)
     expect(result).toEqual(290.642342047956)
-  })
-
-  it('should render component correctly', () => {
-    const wrapper = shallow(<DirectionRenderer />)
-    expect(wrapper.containsAllMatchingElements([
-      <div/>,
-      <canvas id="arpage"></canvas>,
-      <Button/>,
-      <FormGroup/>,
-      <MenuItem/>,
-      <FormControlLabel/>,
-      <Switch/>,
-      <InfoIcon/>
-    ])).toBe.true
   })
 
   it ('Should get distance correctly', () => {
@@ -100,5 +102,39 @@ const DirectionRenderer = directionRendererFactory({
     }
     directionRenderer.getDistance(currentLoc, targetLoc)
   })
+
+  it ('Should handle dial click correctly', () => {
+    const div = document.createElement('div');
+    let props = {
+      coords: {
+        latitude: 0,
+        longitude: 0
+      },
+      targetLoc : {
+        latitude: 100,
+        longitude: 200,
+      },
+      currentLoc :{
+        latitude: 0,
+        longitude: 0
+      }
+    }
+    let directionRenderer = ReactDOM.render(<DirectionRenderer {...props} />, div);
+    let targetLoc = {
+      latitude: 100,
+      longitude: 200,
+    }
+    let currentLoc = {
+      latitude: 0,
+      longitude: 0
+    }
+    let event= {
+      currentTarget: true
+    }
+    console.log(directionRenderer)
+    directionRenderer.handleClick(event)
+    
+  })
+
 })
 
